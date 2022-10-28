9488680f9111549ec532a3c7ff947ea70f2a2d28
moraleseufracio <115846249+moraleseufracio@users.noreply.github.com>
2022-10-28 18:41:13 -0500

    segunda parte

--- ejercicio3tabla.php
+++ ejercicio3tabla.php
@@ -2,16 +2,19 @@
     
     function bucle($n){
     
+        
     if ($n<1 or $n>10) {
         echo "no has escrito un nůmero entre el 1 y el 10.";
         }
+        
     else {
-    echo "<h4>Tabla del $n:</h4>";
+    echo "Tabla del $n: ";
     $i=1;
     while ($i<=10) {
-    echo "$n x $i = ".$n*$i."<br/>";
+    echo "$n x $i = ".$n*$i."";
     $i++;
         }
+        
     }
     }
     
